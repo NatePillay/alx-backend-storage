adf45a185643f9c932fb317b8e293e6298e41cc7
NatePillay <nathan94pillay@gmail.com>
2023-07-23 03:39:58 -0700

    new commit

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,6 +4,7 @@
 import pymongo
 
 def list_all(mongo_collection):
+    ''' lists all documents in collection'''
     if not mongo_collection:
         return []
     return list(mongo_collection.find())
